c521bd0d4746bc0902185756cd513fccb5fdb614
Frank Njoroge <fnjoro29@gmail.com>
2025-03-26 22:48:34 +0300

    add user profile routes

---
+++ backend/routes/userProfileRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { authenticateToken } = require("../middleware/authMiddleware");
+
+//  @route   GET /api/profile
+//  @desc    Get user profile
+//  @access  Private
+router.get("/", authenticateToken, getProfile);
+
+//@route   PUT /api/profile
+//  @desc    Update user profile
+//   @access  Private
+router.put("/", authenticateToken, updateProfile);
+
+module.exports = router;
